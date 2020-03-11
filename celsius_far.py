776b6566279b6c49292ba843d04e657d859a3b91
veenabkatte <60433963+veenabkatte@users.noreply.github.com>
2020-03-12 00:08:32 +0530

    Add files via upload

---
+++ celsius_far.py
@@ -0,0 +1,3 @@
+celsius=37.5
+fahrenhiet=(celsius *1.8)+32
+print("%0.1f degree celsius is equal to %0.1f degree fahrenhiet"%(celsius,fahrenhiet))
---
+++ far_cel.py
@@ -0,0 +1,4 @@
+#program for celsious to fahrenheit
+fahrenhiet=float(input("enter the temprature in fahreanheigt"))
+celsius=(fahrenhiet-32)*5/9
+print("%.2f fahrenhiet is equal to %0.2f degree celsius"%(fahrenhiet,celsius))
